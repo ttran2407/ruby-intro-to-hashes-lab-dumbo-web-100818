309ee318457ad8f997417242c15af6e49b702f2e
Tien Tran <tientran2407.vn@gmail.com>
2018-10-01 21:46:47 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,7 @@ def monopoly_with_third_tier
 	    :name => {
 	      :reading_railroad => {},
         :pennsylvania_railroad => {},
-        
+        :b_and_o_railroad => {},
 	    },
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
