6ca5afc22a74c7c755a8a056a1b8bdfe051cb2eb
Tien Tran <tientran2407.vn@gmail.com>
2018-10-01 21:46:04 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,9 +29,14 @@ def monopoly_with_third_tier
 			monopoly = {
 	  :railroads => {
 	    :pieces => 4,
-	    :name => {},
-	    :rent_in_dollars => {
+	    :name => {
 	      
+	    },
+	    :rent_in_dollars => {
+	      :one_piece_owned => 25,
+      :two_pieces_owned => 50,
+      :three_pieces_owned => 100,
+      :four_pieces_owned => 200
 	    }
 	  }
 		
